2ec35fb13ca134b117f69745170ea769d479fd40
ZEMZEMDEV <74218051+dadavuvu@users.noreply.github.com>
2025-03-25 21:08:42 +0900

    Add files via upload

---
+++ chzzk-anti-grid.user.js
@@ -0,0 +1,57 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// ==UserScript==
+// @name         chzzk-anti-grid
+// @encoding     utf-8
+// @namespace    https://github.com/List-KR/chzzk-anti-grid
+// @homepageURL  https://github.com/List-KR/chzzk-anti-grid
+// @supportURL   https://github.com/List-KR/chzzk-anti-grid/issues
+// @updateURL    https://cdn.jsdelivr.net/npm/@list-kr/chzzk-anti-grid@latest/dist/chzzk-anti-grid.user.js
+// @downloadURL  https://cdn.jsdelivr.net/npm/@list-kr/chzzk-anti-grid@latest/dist/chzzk-anti-grid.user.js
+// @license      MIT
+//
+// @version      2.1.0
+// @author       PiQuark6046 and contributors
+//
+// @match        https://chzzk.naver.com/*
+// @match        https://*.chzzk.naver.com/*
+//
+// @description  치지직 웹 사이트에 있는 그리드 컴퓨팅 프로그램 설치 강제에 대해 우회하는 유저스크립트입니다 
+//
+// @grant        unsafeWindow
+// @run-at       document-start
+// ==/UserScript==
+(()=>{var o=typeof unsafeWindow<"u"?unsafeWindow:window;o.Object.defineProperty=new Proxy(o.Object.defineProperty,{apply(n,t,e){return typeof e[1]=="string"&&e[1]==="isSupportedPlatform"?Reflect.apply(n,t,[e[0],e[1],{value:()=>!1,writable:!1,enumerable:!0,configurable:!0}]):typeof e[1]=="string"&&e[1]==="isSupportedBrowser"?Reflect.apply(n,t,[e[0],e[1],{value:()=>!0,writable:!1,enumerable:!0,configurable:!0}]):Reflect.apply(n,t,e)}});})();
+
+
+}
+/*
+     FILE ARCHIVED ON 15:35:34 Jan 31, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 12:06:51 Mar 25, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 2.357
+  exclusion.robots: 0.015
+  exclusion.robots.policy: 0.007
+  esindex: 0.009
+  cdx.remote: 124.047
+  LoadShardBlock: 489.748 (3)
+  PetaboxLoader3.datanode: 925.882 (4)
+  PetaboxLoader3.resolve: 376.649 (2)
+  load_resource: 841.436
+*/
